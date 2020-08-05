c7db633e10e25121cea68fab2feda14268ad2e4b
Brian Donnoe <dunow33@hotmail.com>
2020-08-05 05:56:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |word1, word2|
+    word1.length <=> word2.length
+  end
+end
+
+def swap_elements(array_swap)
+  temp = array_swap[2]
+  array_swap[2] = array_swap[1]
+  array_swap[1] = temp
+  array_swap
+end
+
+def reverse_array(integers_reverse)
+  reverse_order = []
+  i = integers_reverse.length - 1
+  while(i >= 0) 
+    reverse_order << integers_reverse[i]
+    i -= 1
+  end
+  reverse_order
+end
